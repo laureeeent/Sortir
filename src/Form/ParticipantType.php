769845445f8ea21233262425118be43d28e54f44
alegoanvec2022 <arthur.legoanvec2022@campus-eni.fr>
2023-04-06 15:20:24 +0200

    ADD : cssgggggggggggggggggggggz&i r_)&

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,19 +21,29 @@ class ParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('prenom')
-            ->add('nom')
-            ->add('telephone')
-            ->add('email')
-            ->add('campus', EntityType::class,
-            [
+            ->add('username', TextType::class, [
+                'label' => 'Pseudo'
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Email'
+            ])
+            ->add('campus', EntityType::class, [
                 'class' =>Campus::class,
                 'choice_label' => 'nom'
             ])
             ->add('password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -48,7 +59,7 @@ class ParticipantType extends AbstractType
                 ],
             ])
             ->add('photo', FileType::class, [
-                'label' =>'Photo (PDF file)',
+                'label' =>'Photo',
                 'mapped' => false,
                 'required' => false,
                 'constraints' => [
